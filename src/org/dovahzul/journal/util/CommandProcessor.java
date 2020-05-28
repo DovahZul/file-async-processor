8ff13960c481d6ed47ba33ebaa09011cf5c3c332
dovahzul <tanayris278@gmail.com>
2020-05-28 15:34:22 +0300

    feat: added processig for command SHOW_SIZE_BY_PRICE

--- src/org/dovahzul/journal/util/CommandProcessor.java
+++ src/org/dovahzul/journal/util/CommandProcessor.java
@@ -21,70 +21,74 @@ public class CommandProcessor {
 		o,sell,<size>  (for maximum price)
 	 * @param rawLine
 	 */
-		public static Command createCommand(String rawLine) {
-			
-			Command command = new Command();
-			
-			String requestedOperation = "";
-			CommandType type  = CommandType.values()[0]; // ignore command if UNDEFINED
-			int size = -1; // ignore value for some commands 
-			double price = -1; // ignore value for some commands 
-			
-			List<String> rawValues = Arrays.asList(rawLine.split(","));
-			switch(rawValues.get(0)) {
-			case "u":
-					requestedOperation = rawValues.get(3);
-					switch(requestedOperation) {
-					case "bid":
-						type = CommandType.UPDATE_BID;
-						price = Integer.parseInt(rawValues.get(1));
-						size = Integer.parseInt(rawValues.get(2));
-						break;
-					case "ask":
-						type = CommandType.UPDATE_ASK;
-						price = Integer.parseInt(rawValues.get(1));
-						size = Integer.parseInt(rawValues.get(2));
-						break;
-					default: System.out.println("CommandProcessor: unable to parse line: " + rawLine);
-					}
-				break;
-			case "q":
-					requestedOperation = rawValues.get(1);
-					switch(requestedOperation) {
-					case "best_bid":
-						type = CommandType.SHOW_BEST_BID;
-						break;
-					case "best_ask":
-						type = CommandType.SHOW_BEST_ASK;
-						break;
-					default: System.out.println("CommandProcessor: unable to parse line: " + rawLine);
-					}
-				break;
-			case "o":
-					requestedOperation = rawValues.get(1);
-					switch(requestedOperation){
-					case "buy":
-						type = CommandType.BUY;
-						//price = -1; // by minimum price
-						size = Integer.parseInt(rawValues.get(2));
-						break;
-					case "sell":
-						type = CommandType.SELL;
-						//price = -1 ; // by maximum price
-						size =  Integer.parseInt(rawValues.get(2));
-						break;
-					default: System.out.println("CommandProcessor: unable to parse line: " + rawLine);
-					}
-				break;
-			default: System.out.println("CommandProcessor: unable to parse line: " + rawLine);
-			}
-
-			
-			command.type = type;
-			command.price = price;
-			command.size = size;
-			
-			return command;
-		}
+	public static Command createCommand(String rawLine) {
 		
+		Command command = new Command();
+		
+		String requestedOperation = "";
+		CommandType type  = CommandType.values()[0]; // ignore command if UNDEFINED
+		int size = -1; // ignore when -1
+		double price = -1; // ignore when -1
+		
+		List<String> rawValues = Arrays.asList(rawLine.split(","));
+		switch(rawValues.get(0)) {
+		case "u":
+				requestedOperation = rawValues.get(3);
+				switch(requestedOperation) {
+				case "bid":
+					type = CommandType.UPDATE_BID;
+					price = Integer.parseInt(rawValues.get(1));
+					size = Integer.parseInt(rawValues.get(2));
+					break;
+				case "ask":
+					type = CommandType.UPDATE_ASK;
+					price = Integer.parseInt(rawValues.get(1));
+					size = Integer.parseInt(rawValues.get(2));
+					break;
+				default: System.out.println("CommandProcessor: unable to parse line: " + rawLine);
+				}
+			break;
+		case "q":
+				requestedOperation = rawValues.get(1);
+				switch(requestedOperation) {
+				case "best_bid":
+					type = CommandType.SHOW_BEST_BID;
+					break;
+				case "best_ask":
+					type = CommandType.SHOW_BEST_ASK;
+					break;
+				case "size":
+					type = CommandType.SHOW_SIZE_BY_PRICE;
+					price = Integer.parseInt(rawValues.get(2));
+					break;
+				default: System.out.println("CommandProcessor: unable to parse line: " + rawLine);
+				}
+			break;
+		case "o":
+				requestedOperation = rawValues.get(1);
+				switch(requestedOperation){
+				case "buy":
+					type = CommandType.BUY;
+					//price = -1; // by minimum price
+					size = Integer.parseInt(rawValues.get(2));
+					break;
+				case "sell":
+					type = CommandType.SELL;
+					//price = -1 ; // by maximum price
+					size =  Integer.parseInt(rawValues.get(2));
+					break;
+				default: System.out.println("CommandProcessor: unable to parse line: " + rawLine);
+				}
+			break;
+		default: System.out.println("CommandProcessor: unable to parse line: " + rawLine);
+		}
+
+		
+		command.type = type;
+		command.price = price;
+		command.size = size;
+		
+		return command;
+	}
+	
 }
